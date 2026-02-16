01985f1d777f452cbf59e78b70e855c8423a6517
kirankumarcl9591 <kirankumarcl9591@gmail.com>
2026-02-16 22:53:00 +0530

    Implement ETL process for sales data
    Load, merge, and clean datasets to create final dataset.

--- scripts/etl.py
+++ scripts/etl.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+print("Loading files...")
+
+features = pd.read_csv('../data/Features data set.csv')
+sales = pd.read_csv('../data/sales data-set.csv')
+stores = pd.read_csv('../data/stores data-set.csv')
+
+print("Merging datasets...")
+
+df = sales.merge(stores, on="Store", how="left")
+df = df.merge(features, on=["Store", "Date"], how="left")
+
+print("Cleaning data...")
+df.drop_duplicates(inplace=True)
+df.fillna(0, inplace=True)
+
+df.to_csv('../data/final_dataset.csv', index=False)
+
+print("ETL Done ✅")
